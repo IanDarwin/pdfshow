00c0dcc42493daee273483e455e912bc067547b4
Ian Darwin <ian@darwinsys.com>
2020-07-20 13:07:26 -0400

    Implement simple scaling of documents to fit DocTab...
    Does not preserve ratio, so useless for portrait documents. But 'works for me' :-)

--- src/main/java/net/rejmi/pdfshow/DocTab.java
+++ src/main/java/net/rejmi/pdfshow/DocTab.java
@@ -1,6 +1,7 @@
 package net.rejmi.pdfshow;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.io.File;
@@ -14,6 +15,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.rendering.PDFRenderer;
 
 /** A visual rep of one PDF document, for placing within a TabView */
@@ -31,15 +33,19 @@ class DocTab extends JPanel {
 	File file;
 	/** The PdfBox renderer for it */
 	PDFRenderer renderer;
+	/** Scaling to make the document fit */
+	float scaleX, scaleY;
 	/** Our user's annotations for this doc, indexed by page# */
 	List<GObject>[] addIns;
 	
+	/** Construct one Document Tab */
 	DocTab(File file) throws IOException {
 		super();
 
 		// PDF stuff
 		this.file = file;
 		this.doc = PDDocument.load(file);
+
 		pageCount = doc.getNumberOfPages();
 		renderer = new PDFRenderer(doc);
 		addIns = new List[pageCount];
@@ -55,7 +61,21 @@ class DocTab extends JPanel {
 			setPageNumber(e.getValue());
 		});
 		add(sbar, BorderLayout.EAST);
-		// setSize(800, 800);
+	}
+
+	/**
+	 * Scale PDF to fit - after main window is fully sized.
+	 * Scaling is quick & dirty: works for slide decks, not for books(!).
+	 * XXX Must either constrain ratio or allow to manually adjust.
+	 */
+	void computeScaling() {
+		final PDRectangle pdfBBox = doc.getPage(0).getBBox();
+		System.out.println("BBox: " + pdfBBox);
+		final Dimension compSize = pdfComponent.getSize();
+		System.out.println("Component size = " + compSize);
+		scaleX = (float)(compSize.getWidth() / pdfBBox.getWidth());
+		scaleY = (float)(compSize.getHeight() / pdfBBox.getHeight());
+		System.out.println("Computed scaling as " + scaleX + "," + scaleY);
 	}
 
 	void setPageNumber(int page) {
@@ -115,7 +135,10 @@ class DocTab extends JPanel {
 		super.paintComponent(g);
 		try {
 			// 2) PdfBox - whole page
-			renderer.renderPageToGraphics(pageNumber, (Graphics2D) g);
+			if (scaleX == 0) {
+				computeScaling();
+			}
+			renderer.renderPageToGraphics(pageNumber, (Graphics2D) g, scaleX, scaleY);
 			// 3) Our annotations, if any
 			if (addIns[pageNumber] != null) {
 				for (GObject obj : addIns[pageNumber]) {
