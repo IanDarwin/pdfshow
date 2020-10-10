6205954a083f5d6d810aa0d3a81b2e475c74bc40
Ian Darwin <ian@darwinsys.com>
2020-10-10 14:41:33 -0400

    Better name for mock object

--- src/test/java/net/rejmi/pdfshow/ContainmentTest.java
+++ src/test/java/net/rejmi/pdfshow/ContainmentTest.java
@@ -10,47 +10,47 @@ import org.junit.Test;
 public class ContainmentTest {
 
 	// Because GObject is abstract.
-	class TestGObject extends GObject{
-		TestGObject(int x, int y, int width, int height) {
+	class MockGObject extends GObject{
+		MockGObject(int x, int y, int width, int height) {
 			super(x, y, width, height);
 		}
 
 		@Override
 		void render(Graphics g) {
-			throw new IllegalStateException("render() called on mock GObject");
+			throw new IllegalStateException("render() called on MockGObject");
 		}
 	};
 
 	@Test
 	public void testContainsULT() {
-		assertTrue ("ULT", Containment.contains(new TestGObject(100, 100, -160, -200), -50, -50));
+		assertTrue ("ULT", Containment.contains(new MockGObject(100, 100, -160, -200), -50, -50));
 	}
 	@Test
 	public void testContainsULF() {
-		assertFalse("ULF", Containment.contains(new TestGObject(100, 100, -160, -200), -150, -150));
+		assertFalse("ULF", Containment.contains(new MockGObject(100, 100, -160, -200), -150, -150));
 	}
 	@Test
 	public void testContainsURT() {
-		assertTrue("URT", Containment.contains(new TestGObject(100, 100, +160, -200), +150, -50));
+		assertTrue("URT", Containment.contains(new MockGObject(100, 100, +160, -200), +150, -50));
 	}
 	@Test
 	public void testContainsURF() {
-		assertFalse("URF", Containment.contains(new TestGObject(100, 100, +160, -200), +300, -150));
+		assertFalse("URF", Containment.contains(new MockGObject(100, 100, +160, -200), +300, -150));
 	}
 	@Test
 	public void testContainsLLT() {
-		assertTrue("LLT", Containment.contains(new TestGObject(100, 100, -160, +200), -150, 150));
+		assertTrue("LLT", Containment.contains(new MockGObject(100, 100, -160, +200), -150, 150));
 	}
 	@Test
 	public void testContainsLLF() {
-		assertFalse("LLF", Containment.contains(new TestGObject(100, 100, -160, +200), -300, 150));
+		assertFalse("LLF", Containment.contains(new MockGObject(100, 100, -160, +200), -300, 150));
 	}
 	@Test
 	public void testContainsLRT() {
-		assertTrue("LRT", Containment.contains(new TestGObject(100, 100, +160, +200), 150, 150));
+		assertTrue("LRT", Containment.contains(new MockGObject(100, 100, +160, +200), 150, 150));
 	}
 	@Test
 	public void testContainsLRF() {
-		assertFalse("LRF", Containment.contains(new TestGObject(100, 100, +160, +200), 300, 150));
+		assertFalse("LRF", Containment.contains(new MockGObject(100, 100, +160, +200), 300, 150));
 	}
 }
