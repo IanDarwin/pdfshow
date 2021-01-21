93be96582985cbeaf7c1344257c8a15bcdd0e7f5
Ian Darwin <ian@darwinsys.com>
2021-01-21 12:43:13 -0500

    Formatting. 'x' -> 'X'. Extract 'label' variable.

--- src/main/java/net/rejmi/pdfshow/ClosableTabHeader.java
+++ src/main/java/net/rejmi/pdfshow/ClosableTabHeader.java
@@ -15,8 +15,9 @@ final class ClosableTabHeader extends JPanel {
 
 	public ClosableTabHeader(Consumer<DocTab> tabCloser, DocTab docTab) {
 		setLayout(new FlowLayout());
-		add(new JLabel(docTab.file.getName()));
-		// Leave comment out - tooltip eats mouse event so switching fails
+        JLabel label = new JLabel(docTab.file.getName());
+        add(label);
+		// Leave commented out - tooltip eats mouse event so switching fails
 		// See https://stackoverflow.com/questions/19910739 but applying that didn't work(?)
 		// setToolTipText(docTab.file.getAbsolutePath());
 		JButton xButton = new MyCloseButton();
@@ -26,20 +27,20 @@ final class ClosableTabHeader extends JPanel {
 	}
 
 	// The X button for tabs
-	class MyCloseButton extends JButton {
-		private static final long serialVersionUID = 1L;
+    private static class MyCloseButton extends JButton {
+        private static final long serialVersionUID = 1L;
 
-		public MyCloseButton() {
-		    super("x");
-		    setBorder(BorderFactory.createEmptyBorder());
-		    setFocusPainted(false);
-		    setBorderPainted(false);
-		    setContentAreaFilled(false);
-		    setRolloverEnabled(false);
-		  }
-		  @Override
-		  public Dimension getPreferredSize() {
-		    return new Dimension(16, 16);
-		  }
-		};
+        public MyCloseButton() {
+            super("X");
+            setBorder(BorderFactory.createEmptyBorder());
+            setFocusPainted(false);
+            setBorderPainted(false);
+            setContentAreaFilled(false);
+            setRolloverEnabled(false);
+        }
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(16, 16);
+        }
+    }
 }
