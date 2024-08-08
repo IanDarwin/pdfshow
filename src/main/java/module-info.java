9f2b67e75cf7009ad6d8df5ce76abd6c64ac905c
Ian Darwin <ian@darwinsys.com>
2024-08-07 20:22:22 -0400

    First working version of a module-info for this project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module net.rejmi.pdfshow {
+
+	requires java.base;
+	requires java.desktop;
+	requires java.prefs;
+	requires java.logging;
+	requires darwinsys.api;
+
+	requires org.apache.pdfbox;
+
+	exports net.rejmi.pdfshow;
+}
